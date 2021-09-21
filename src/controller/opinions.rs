a503d6fde61c967d67941503d4738a7c05ef769d
Hubert Jaremko <hjaremko@outlook.com>
2021-09-21 17:00:50 +0100

    Use common database pool, add courses endpoint

--- src/controller/opinions.rs
+++ src/controller/opinions.rs
@@ -1,31 +1,17 @@
-use crate::repo::{beasts, opinions};
+use crate::repo::opinions;
+use sqlx::SqlitePool;
 use tide::{Body, Request, Response, Server};
 
-pub fn bind_opinions(server: &mut Server<()>) {
-    server.at("/api/v1/opinions").get(all_opinions);
+pub fn bind_opinions(server: &mut Server<()>, pool: SqlitePool) {
+    server.at("/api/v1/opinions").get(move |_req| {
+        let pool = pool.clone();
+        all_opinions(_req, pool)
+    });
 }
 
-async fn all_opinions(_req: Request<()>) -> tide::Result {
-    let opinions = opinions::get_all().await?;
+async fn all_opinions(_req: Request<()>, pool: SqlitePool) -> tide::Result {
+    let opinions = opinions::get_all(pool).await?;
     let mut req = Response::new(tide::StatusCode::Ok);
     req.set_body(Body::from_json(&opinions)?);
-    Ok(req)
-}
-
-pub fn bind_beasts(server: &mut Server<()>) {
-    server.at("/api/v1/beasts")
-        .get(all_beasts)
-    // .post(new_beast)
-    ;
-    // todo
-    // server.at("/api/v1/beasts/:id/").get(all_opinions);
-    // server.at("/api/v1/beasts/:id/opinions").get(all_opinions);
-    // server.at("/api/v1/beasts/:id/courses").get(all_opinions);
-}
-
-async fn all_beasts(_req: Request<()>) -> tide::Result {
-    let beasts = beasts::get_all().await?;
-    let mut req = Response::new(tide::StatusCode::Ok);
-    req.set_body(Body::from_json(&beasts)?);
     Ok(req)
 }
--- src/main.rs
+++ src/main.rs
@@ -2,17 +2,22 @@ pub mod controller;
 pub mod model;
 pub mod repo;
 
+use crate::controller::beasts::bind_beasts;
+use crate::controller::courses::bind_courses;
 use crate::controller::home_page::bind_home_page;
-use crate::controller::opinions::{bind_beasts, bind_opinions};
+use crate::controller::opinions::bind_opinions;
+use sqlx::SqlitePool;
+use std::env;
 
 #[async_std::main]
 async fn main() -> tide::Result<()> {
     tide::log::start();
-
+    let pool = SqlitePool::connect(&env::var("DATABASE_URL")?).await?;
     let mut app = tide::new();
     bind_home_page(&mut app);
-    bind_opinions(&mut app);
-    bind_beasts(&mut app);
+    bind_opinions(&mut app, pool.clone());
+    bind_beasts(&mut app, pool.clone());
+    bind_courses(&mut app, pool.clone());
     app.listen("127.0.0.1:8080").await?;
     Ok(())
 }
